847320d046fa9f03993d5245b230bd4c2cd3188a
IVBadalov21 <IVBadalov21@codingburgas.bg>
2025-06-01 19:16:38 +0300

    Debug

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from app.models import db
 from app.auth import login_manager
 from app.main.routes import main_bp
-app.register_blueprint(main_bp)
 
 def create_app():
     app = Flask(__name__)
@@ -12,6 +11,7 @@ def create_app():
     db.init_app(app)
     login_manager.init_app(app)
 
+    app.register_blueprint(main_bp)
     from app.auth.routes import auth_bp
     app.register_blueprint(auth_bp)
 
